000e72e9c6ce2ebe07a90d39eeb24ecc8bdcd3bc
carollkarry <carollkarry@cug.edu.cn>
2023-04-21 20:54:15 +0800

    Initial commit

---
+++ src/main/java/com/example/server/cugerhuobackstage/CugErHuoBackStageApplication.java
@@ -0,0 +1,13 @@
+package com.example.server.cugerhuobackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CugErHuoBackStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CugErHuoBackStageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/server/cugerhuobackstage/CugErHuoBackStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server.cugerhuobackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CugErHuoBackStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
